e527a5625bcd7aa9aa7af28bc9f03a0ed4cae3a6
Tenplant <tenplant1203@gmail.com>
2026-05-12 16:49:21 +0900

    feat: handle abort and race conditions

--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -1,11 +1,19 @@
 import { routes } from "./routes";
 import { NativeRouterProvider } from "../lib/react/NativeRouterProvider";
 import { Outlet } from "../lib/react/Outlet";
+import { Link } from "../lib/react/Link";
 
 export function App() {
   return (
     <NativeRouterProvider routes={routes}>
       <h1>Browser Native Router Demo</h1>
+      <nav style={{ display: "flex", gap: 12 }}>
+        <Link to="/">Home</Link>
+        <Link to="/items/1">Item 1</Link>
+        <Link to="/items/2">Item 2</Link>
+        <Link to="/items/3">Item 3</Link>
+        <Link to="/search">Search</Link>
+      </nav>
       <Outlet />
     </NativeRouterProvider>
   );
